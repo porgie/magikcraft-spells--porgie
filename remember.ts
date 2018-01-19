c3102373c042651133bb6c49b79e0f2c6a60286c
porgie <iamcharmainechalmers@gmail.com>
2018-01-19 00:39:12 +0000

    Update remember.ts

--- remember.ts
+++ remember.ts
@@ -1,6 +1,6 @@
 const magik = magikcraft.io;
 
 function remember(name){
-    const memory = magik.memento.getItem(name);
-    magik.dixit(memory);
+    const here = magik.hic();
+    magik.memento.setItem(name, here);
 }
